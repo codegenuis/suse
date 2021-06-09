bf6b2046d8ddf75388987ea6e7ace930be1e8be1
root <root@TJ-LP-001-0084.Taj.local>
2021-06-09 16:57:58 +0100

    added the metrics and status endpoints url

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/status', methods=['GET'])
+def api_all():
+    return jsonify({"result": "OK - healthy"})
+
+@app.route('/metrics', methods=['GET'])
+def get_metrics():
+    return jsonify({"data": {"UserCount": 140, "UserCountActive": 23}})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
